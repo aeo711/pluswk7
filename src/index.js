02a1c312ff17ffe6c4c144be143461703f27d626
aeo711 <adreaojukwu@gmail.com>
2025-01-12 05:53:10 -0600

    Corrected 2 errors. Still need to Debug JS Function
    The usability test is still not working.

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 function handleSearchSubmit(event) {
   event.preventDefault();
   let searchInput = document.querySelector("#search-form-input");
-  let city = document.querySelector("#city");
+  let cityElement = document.querySelector("#city");
   cityElement.innerHTML = searchInput.value;
 }
 
